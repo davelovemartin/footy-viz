cffc843ef6ab54a95944a580fb752da6f4672e22
davelovemartin <dave.martin@cxpartners.co.uk>
2021-06-06 17:30:08 +0100

    adding chart components

--- pages/api/test.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const online = true
-
-  if (online === true) {
-    res.status(200).json({ status: 'online' })
-  } else {
-    res.status(404).json({ message: 'not found' })
-  }
-}
